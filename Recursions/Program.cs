472675b534a2c59381839ca142746b4ad2989b86
Sergey <melnikov_s_a@mail.ru>
2022-11-17 18:00:15 +0300

    StolenAkkerman

--- Recursions/Program.cs
+++ Recursions/Program.cs
@@ -37,9 +37,27 @@ void SumBetween(int number1, int number2)
         number2--;
 
     }
-    Console.WriteLine($"{sum} ");
+    Console.WriteLine(sum);
 
 }
 
 // Задача 3. Напишите программу вычисления функции Аккермана
 // с помощью рекурсии. Даны два неотрицательных числа m и n.
+// (Здесь сам понять не смог уже на уровне функции, поэтому взял в Сети)
+
+Console.WriteLine("Задача 3:");
+int numM3 = 3;
+int numN3 = 2;
+Console.WriteLine(Akkerman (numM3, numN3)); 
+
+int Akkerman(int number1, int number2)
+{
+  if (number1 == 0)
+    return number2 + 1;
+  else
+    if ((number1 != 0) && (number2 == 0))
+      return Akkerman(number1 - 1, 1);
+    else
+      return Akkerman(number1 - 1, Akkerman(number1, number2 - 1));
+
+}
