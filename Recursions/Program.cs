8c142ac56796ea48cc12e450065da7a664e035ea
Sergey <melnikov_s_a@mail.ru>
2022-11-17 16:26:53 +0300

    FirstRecTry

---
+++ Recursions/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1 Задайте значение N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+Console.WriteLine("Здравствуйте! Введите число N");
+int numN = int.Parse(Console.ReadLine());
+
+void ToOneRecursion(int number)
+{
+    if (number>0)
+    {
+    Console.Write($"{number}, ");
+    number--;
+    ToOneRecursion(number);
+    }
+}
